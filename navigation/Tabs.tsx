c4d0e084ea9ad4b15f969bc9c61de2af2ff778c0
Hoosss <hsnam1211@naver.com>
2023-06-08 02:10:35 +0900

    .

--- navigation/Tabs.tsx
+++ navigation/Tabs.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { View, Text, useColorScheme, Platform, Pressable } from 'react-native';
+import { View, Text, useColorScheme, Platform, Pressable, Image } from 'react-native';
 
 import { useNavigation, useIsFocused } from '@react-navigation/native';
 import { useRecoilState } from 'recoil';
@@ -10,17 +10,18 @@ import Sample from '../screens/Sample';
 
 const Tab = createBottomTabNavigator();
 
+
 function Tabs() {
   return (
     <Tab.Navigator
       screenOptions={{
         tabBarHideOnKeyboard: true,
-        headerShown: false,
+        headerShown: true,
         tabBarStyle: {
           // backgroundColor: 'transparent',
           // backgroundColor: BLACK_COLOR,
 
-          backgroundColor: '#000',
+          backgroundColor: '#002B5B',
           position: 'absolute',
           left: 0,
           bottom: 0,
@@ -31,13 +32,13 @@ function Tabs() {
           paddingBottom:
             Platform.OS === 'ios' ? (DeviceInfo.hasNotch() ? 26 : 12) : 12,
         },
-        tabBarActiveTintColor: 'white',
+        tabBarActiveTintColor: '#F9F5EB',
         headerStyle: {
-          backgroundColor: 'white',
+          backgroundColor: '#002B5B',
         },
         headerTitleStyle: {
           // color: BLACK_COLOR,
-          color: '#000',
+          color: '#F9F5EB',
           fontSize: 20,
           fontWeight: '500',
         },
@@ -45,7 +46,7 @@ function Tabs() {
       }}
     >
       <Tab.Screen
-        name='Main'
+        name='해우소'
         component={Main}
         options={{
           // tabBarIcon: ({ focused, color, size }) => {
@@ -59,16 +60,20 @@ function Tabs() {
           // },
           // headerRight: () =>
           //   isLogin ? <CustomAlarmButton path={'Home'} /> : null,
-          tabBarLabel: 'MAIN',
+          // tabBarLabel: () => { return <Text>dd</Text> },
+          headerShadowVisible: false, // applied here
           tabBarLabelStyle: {
             fontSize: 12,
           },
+          // headerTitle: () => (
+          //   <Image style={{ width: 50, height: 50 }} source={require("./logo.png")} />
+          // ),
         }}
-        // listeners={{
-        //   tabPress: (e) => {
-        //     console.log(e);
-        //   },
-        // }}
+      // listeners={{
+      //   tabPress: (e) => {
+      //     console.log(e);
+      //   },
+      // }}
       />
       <Tab.Screen
         name='Sample'
--- screens/Main.tsx
+++ screens/Main.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext, useRef } from 'react';
 
-import { View, Text, Pressable, AppState, AppStateStatus } from 'react-native';
+import { View, Text, Pressable, AppState, AppStateStatus, FlatList } from 'react-native';
 import styled from 'styled-components';
 import { Storage } from '../src/util/storage'
 import { getMessageState } from '../src/recoil/atoms';
@@ -8,8 +8,8 @@ import messaging from '@react-native-firebase/messaging';
 import { width, height } from '../src/util/screenDimensions';
 
 const Container = styled(View)`
-  margin-right: 12;
-  margin-left: 12;
+  margin-right: 0px;
+  margin-left: 0px;
 `;
 
 function Main() {
@@ -70,25 +70,102 @@ function Main() {
     return () => subscribeToMessages();
   }, [])
 
+  const data = [
+    { id: '1', title: 'Item 1' },
+    { id: '2', title: 'Item 2' },
+    { id: '3', title: 'Item 3' },
+    { id: '4', title: 'Item 4' },
+    { id: '5', title: 'Item 5' },
+    { id: '6', title: 'Item 1' },
+    { id: '7', title: 'Item 2' },
+    { id: '8', title: 'Item 3' },
+    { id: '9', title: 'Item 4' },
+    { id: '10', title: 'Item 5' },
+  ];
+
+  const PushCountComponent = () => {
+    return (
+      <View style={{ padding: 16, backgroundColor: '#002B5B', width: width }}>
+        <Text style={{ color: 'white', textAlign: 'center' }}>
+          🚽 오늘 해우소 사용 가능 횟수는 총 5회 🚽
+        </Text>
+      </View>
+    );
+  };
+
+  const Header = () => {
+    return (
+      <View>
+        <View style={{
+          elevation: 5, // 안드로이드 그림자
+          shadowColor: '#000', // iOS 그림자 색상
+          shadowOffset: { width: 0, height: 3 }, // iOS 그림자 오프셋
+          shadowOpacity: 0.8, // iOS 그림자 투명도
+          shadowRadius: 4, // iOS 그림자 반경,
+          backgroundColor: '#002B5B', width: width - 24, borderRadius: 8, height: 150, marginTop: 15, marginBottom: 10,
+          justifyContent: 'center'
+        }}>
+          <Text style={{ color: 'white', textAlign: 'center', marginBottom: 20 }}>
+            해우소 사용 가능 횟수를 다 소진하셨다면...
+          </Text>
+          <Text style={{ color: 'white', textAlign: 'center', fontSize: 20, fontWeight: 'bold' }}>
+            광고 보고 다시 근심 털기 📺
+          </Text>
+        </View>
+        <View style={{ marginTop: 20, marginBottom: 10 }}>
+          <Text style={{ fontWeight: 'bold', fontSize: 25, }}>
+            공개 된 근심 거리
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
+  const Footer = () => {
+    return (
+      <View style={{ padding: 16, backgroundColor: 'lightgray' }}>
+        <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Footer</Text>
+      </View>
+    );
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={{ padding: 16, width: width - 24, height: 80, marginTop: 7, marginBottom: 7, backgroundColor: '#E4DCCF', borderRadius: 6 }}>
+        <Text>{item.title}</Text>
+      </View>
+    );
+  };
+
+  const MainFlatList = () => {
+    return (
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        // ListFooterComponent={<Footer />}
+        ListHeaderComponent={<Header />}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ marginLeft: 12 }}
+      />
+    );
+  };
+
   return (
     <View
       style={{
-        backgroundColor: '#25282b',
+        backgroundColor: '#F9F5EB',
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
       }}
     >
       <Container>
-      <View>
-        <Text style={{color: 'white'}}>
-          남은 답장 횟수 : 0/5
-        </Text>
-      </View>
-      {message?.title && <Pressable onPress={() => { setMessage(undefined) }} style={{ borderRadius: 6, padding: 20, width: width - 40, height: 400, backgroundColor: 'gray' }}>
-        <Text style={{ color: '#fff' }}>{message?.title}</Text>
-        <Text style={{ color: '#fff' }}>{message?.body}</Text>
-      </Pressable>}
+        <PushCountComponent />
+        <MainFlatList />
+        {message?.title && <Pressable onPress={() => { setMessage(undefined) }} style={{ borderRadius: 6, padding: 20, width: width - 40, height: 400, backgroundColor: 'gray' }}>
+          <Text style={{ color: '#fff' }}>{message?.title}</Text>
+          <Text style={{ color: '#fff' }}>{message?.body}</Text>
+        </Pressable>}
       </Container>
     </View>
   );
